682742c96cff9869887c728c344a963090d0185b
Andrea Ramirez <andrea.anza3@gmail.com>
2024-07-12 11:20:30 -0600

    Added nav links smooth scrolling

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1')
+///////////////////////////////////////
+// Modal window
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -29,3 +30,31 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+// Button scrolling
+btnScrollTo.addEventListener('click', function(e){
+  section1.scrollIntoView({behavior: 'smooth'})
+})
+
+// Page navigation
+// document.querySelectorAll('.nav__link').forEach(function (element) {
+//   element.addEventListener('click', function(e) {
+//   e.preventDefault();
+//   const id = this.getAttribute('href');
+//   console.log(id);
+//   document.querySelector(id).scrollIntoView({behavior:'smooth'})
+// }) })
+
+//  1. Add event listener to commont parent element
+//  2. Determine what element iriginated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e){
+  e.preventDefault();
+
+  // Matching strategy
+  if(e.target.classList.contains('nav__link')){
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({behavior:'smooth'})
+  }
+})
