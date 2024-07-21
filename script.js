69cab73afc5f2cd3016b80e4ad82d6cae65e991f
Andrea Ramirez <andrea.anza3@gmail.com>
2024-07-21 00:16:15 -0600

    Tabbed component is now working

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1')
+const section1 = document.querySelector('#section--1');
 ///////////////////////////////////////
 // Modal window
 
@@ -31,11 +31,10 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-
 // Button scrolling
-btnScrollTo.addEventListener('click', function(e){
-  section1.scrollIntoView({behavior: 'smooth'})
-})
+btnScrollTo.addEventListener('click', function (e) {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
 
 // Page navigation
 // document.querySelectorAll('.nav__link').forEach(function (element) {
@@ -49,12 +48,35 @@ btnScrollTo.addEventListener('click', function(e){
 //  1. Add event listener to commont parent element
 //  2. Determine what element iriginated the event
 
-document.querySelector('.nav__links').addEventListener('click', function (e){
+document.querySelector('.nav__links').addEventListener('click', function (e) {
   e.preventDefault();
 
   // Matching strategy
-  if(e.target.classList.contains('nav__link')){
+  if (e.target.classList.contains('nav__link')) {
     const id = e.target.getAttribute('href');
-    document.querySelector(id).scrollIntoView({behavior:'smooth'})
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
-})
+});
+
+// Tabbed Component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabContents = document.querySelectorAll('.operations__content');
+// tabs.forEach(tab=> tab.addEventListener('click', ()=>console.log('TAB'))) // This is a bad practice
+tabsContainer.addEventListener('click', (e) => {
+  const clicked = e.target.closest('.operations__tab');
+
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach((tab) => tab.classList.remove('operations__tab--active'));
+  tabContents.forEach((content) =>
+    content.classList.remove('operations__content--active')
+  );
+
+  // Activate Content Area
+  clicked.classList.add('operations__tab--active');
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
